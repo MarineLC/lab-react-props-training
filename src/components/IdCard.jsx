d37bc95cda3c61633e798c1d96e0833ad77577ba
Marine Le Cleach <lecleach.m@gmail.com>
2023-12-11 16:19:06 +0100

    1st iteration

---
+++ src/components/IdCard.jsx
@@ -0,0 +1,16 @@
+function IdCard (props) {
+
+    //{lastName}, {firstName}, {gender}, {height}, {birth}, {picture}
+    
+    <div className="IdCard">
+      <p>lastName</p>
+      <p>firstName</p>
+      <p>gender</p>
+      <p>height</p>
+      <p>birth</p>
+      <p>picture</p>
+      </div>
+    
+    }
+    
+    export default IdCard;
